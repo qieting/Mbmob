52b747283ce1b44fc67296583013ec30ada4dd92
qieting <1509134296@qq.com>
2017-09-15 09:59:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mbomb1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mbomb1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
+
+//9.12
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //初始化
+        Bmob.initialize(this, "cc37bf2e541f68b7ac5014c4153a1019");
+        // 注:自v3.5.2开始，数据sdk内部缝合了统计sdk，开发者无需额外集成，传渠道参数即可，不传默认没开启数据统计功能
+        //Bmob.initialize(this, "Your Application ID","bmob");
+
+        //增加数据
+        Person p2 = new Person();
+        p2.setName("luck");
+        p2.setAddress("北京海淀");
+        p2.save(new SaveListener<String>() {
+            @Override
+            public void done(String objectId,BmobException e) {
+                if(e==null){
+                    Toast.makeText(getBaseContext(),"添加数据成功，返回objectId为："+objectId,Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getBaseContext(),"创建数据失败：" + e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+}
